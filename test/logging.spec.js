26ddc15841fc8634b5bf3c37f6e050d6606d7b1f
Frank Thelen <frank.thelen@telefonica.com>
2017-12-30 13:19:05 +0100

    more unit test

--- test/logging.spec.js
+++ test/logging.spec.js
@@ -35,6 +35,24 @@ describe('Rules.evaluate() / delegate logging', () => {
     rools.evaluate({ user: frank, weather: good });
     expect(counter).to.not.be.equals(0);
   });
+
+  it('should log errors by default', () => {
+    const brokenRule = {
+      name: 'broken rule #2',
+      when: () => true, // fire immediately
+      then: (facts) => {
+        facts.bla.blub = 'blub'; // TypeError: Cannot read property 'blub' of undefined
+      },
+    };
+    let counter = 0;
+    const spy = () => {
+      counter += 1;
+    };
+    const rools = new Rools({ logDelegate: spy });
+    rools.register(brokenRule, ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome);
+    rools.evaluate({ user: frank, weather: good });
+    expect(counter).to.not.be.equals(0);
+  });
 });
 
 describe('Rules.evaluate() / console logging', () => {
@@ -64,6 +82,34 @@ describe('Rules.evaluate() / console logging', () => {
       },
     };
     const rools = new Rools({ logErrors: true, logDebug: false });
+    rools.register(brokenRule, ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome);
+    rools.evaluate({ user: frank, weather: good });
+    expect(console.error).to.be.called; // eslint-disable-line no-unused-expressions, no-console
+  });
+
+  it('should log errors by default', () => {
+    const brokenRule = {
+      name: 'broken rule #2',
+      when: () => true, // fire immediately
+      then: (facts) => {
+        facts.bla.blub = 'blub'; // TypeError: Cannot read property 'blub' of undefined
+      },
+    };
+    const rools = new Rools();
+    rools.register(brokenRule, ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome);
+    rools.evaluate({ user: frank, weather: good });
+    expect(console.error).to.be.called; // eslint-disable-line no-unused-expressions, no-console
+  });
+
+  it('should log errors by default / 2', () => {
+    const brokenRule = {
+      name: 'broken rule #2',
+      when: () => true, // fire immediately
+      then: (facts) => {
+        facts.bla.blub = 'blub'; // TypeError: Cannot read property 'blub' of undefined
+      },
+    };
+    const rools = new Rools({});
     rools.register(brokenRule, ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome);
     rools.evaluate({ user: frank, weather: good });
     expect(console.error).to.be.called; // eslint-disable-line no-unused-expressions, no-console
