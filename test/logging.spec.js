316b16737f1936df0cb18366667cf996989a2bad
Frank Thelen <frank.thelen@telefonica.com>
2017-12-30 02:39:51 +0100

    unit tests on logging

---
+++ test/logging.spec.js
@@ -0,0 +1,71 @@
+const Rools = require('../src');
+const { frank } = require('./facts/users')();
+const { good } = require('./facts/weather')();
+const {
+  ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome,
+} = require('./rules/mood');
+require('./setup');
+
+describe('Rules.evaluate() / delegate logging', () => {
+  it('should log debug', () => {
+    let counter = 0;
+    const spy = () => {
+      counter += 1;
+    };
+    const rools = new Rools({ logErrors: false, logDebug: true, logDelegate: spy });
+    rools.register(ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome);
+    rools.evaluate({ user: frank, weather: good });
+    expect(counter).to.not.be.equals(0);
+  });
+
+  it('should log errors', () => {
+    const brokenRule = {
+      name: 'broken rule #2',
+      when: () => true, // fire immediately
+      then: (facts) => {
+        facts.bla.blub = 'blub'; // TypeError: Cannot read property 'blub' of undefined
+      },
+    };
+    let counter = 0;
+    const spy = () => {
+      counter += 1;
+    };
+    const rools = new Rools({ logErrors: true, logDebug: false, logDelegate: spy });
+    rools.register(brokenRule, ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome);
+    rools.evaluate({ user: frank, weather: good });
+    expect(counter).to.not.be.equals(0);
+  });
+});
+
+describe('Rules.evaluate() / console logging', () => {
+  beforeEach(() => {
+    sinon.spy(console, 'log');
+    sinon.spy(console, 'error');
+  });
+
+  afterEach(() => {
+    console.log.restore(); // eslint-disable-line no-console
+    console.error.restore(); // eslint-disable-line no-console
+  });
+
+  it('should log debug', () => {
+    const rools = new Rools({ logErrors: false, logDebug: true });
+    rools.register(ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome);
+    rools.evaluate({ user: frank, weather: good });
+    expect(console.log).to.be.called; // eslint-disable-line no-unused-expressions, no-console
+  });
+
+  it('should log errors', () => {
+    const brokenRule = {
+      name: 'broken rule #2',
+      when: () => true, // fire immediately
+      then: (facts) => {
+        facts.bla.blub = 'blub'; // TypeError: Cannot read property 'blub' of undefined
+      },
+    };
+    const rools = new Rools({ logErrors: true, logDebug: false });
+    rools.register(brokenRule, ruleMoodGreat, ruleMoodSad, ruleGoWalking, ruleStayAtHome);
+    rools.evaluate({ user: frank, weather: good });
+    expect(console.error).to.be.called; // eslint-disable-line no-unused-expressions, no-console
+  });
+});
