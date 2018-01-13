fc7190acc56c00d777ab72be038134565d89e217
Frank Thelen <frank.thelen@telefonica.com>
2018-01-13 12:45:35 +0100

    rule asserts // minor

--- src/Rule.js
+++ src/Rule.js
@@ -14,12 +14,11 @@ class Rule {
 
   assert() {
     const isFunc = func => typeof func === 'function';
-    const isArray = array => Array.isArray(array);
     assert(this.name, 'rule "name" is required');
     assert(this.when, `rule "when" is required: "${this.name}"`);
     assert(this.then, `rule "then" is required: "${this.name}"`);
     assert(
-      isFunc(this.when) || (isArray(this.when) && !this.when.filter(f => !isFunc(f)).length),
+      !this.when.filter(premise => !isFunc(premise)).length,
       `rule "when" must be a function or an array of functions: "${this.name}"`,
     );
     assert(
